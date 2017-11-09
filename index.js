ecf95dca9464585976316107d04c5b93eb69aa75
Obi <obinnawalker@gmail.com>
2017-11-09 16:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function deleteFromDriverByKey(driver, key){
   delete newDriver[key]
   return newDriver
 }
-function destructivelyDeleteFromObjectByKey(driver, key){
+function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver.key
   return driver
 }
